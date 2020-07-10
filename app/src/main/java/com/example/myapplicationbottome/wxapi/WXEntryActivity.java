457f5c5dc2cebf9b8b1c2a61c447612ecb1ada87
toryzhou <522617505@qq.com>
2020-07-10 17:53:59 +0800

    Update for wechat

---
+++ app/src/main/java/com/example/myapplicationbottome/wxapi/WXEntryActivity.java
@@ -0,0 +1,54 @@
+package com.example.myapplicationbottome.wxapi;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+import com.tencent.mm.opensdk.modelbase.BaseReq;
+import com.tencent.mm.opensdk.modelbase.BaseResp;
+import com.tencent.mm.opensdk.modelmsg.SendAuth;
+import com.tencent.mm.opensdk.openapi.IWXAPI;
+import com.tencent.mm.opensdk.openapi.IWXAPIEventHandler;
+import com.tencent.mm.opensdk.openapi.WXAPIFactory;
+
+public class WXEntryActivity extends Activity implements IWXAPIEventHandler {
+
+    private IWXAPI iwxapi;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // 隐藏状态栏
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        //接收到分享以及登录的intent传递handleIntent方法，处理结果
+        iwxapi = WXAPIFactory.createWXAPI(this, "wx483a9c377584702d", false);
+        iwxapi.handleIntent(getIntent(), this);
+
+    }
+
+    @Override
+    public void onReq(BaseReq baseReq) {
+    }
+
+    @Override
+    public void onResp(BaseResp baseResp) {
+        //登录回调
+        switch (baseResp.errCode) {
+            case BaseResp.ErrCode.ERR_OK:
+                String code = ((SendAuth.Resp) baseResp).code;
+                //获取的code和后台交换信息
+                
+                break;
+            case BaseResp.ErrCode.ERR_AUTH_DENIED://用户拒绝授权
+                finish();
+                break;
+            case BaseResp.ErrCode.ERR_USER_CANCEL://用户取消
+                finish();
+                break;
+            default:
+                finish();
+                break;
+        }
+    }
+}
